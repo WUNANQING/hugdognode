2e83f40b9e873577a9102d758cc07a4671063275
WU NAN QING <a1993112@gmail.com>
2020-03-19 18:34:27 +0800

    商品資料和商品細節

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,17 @@
 var express = require("express");
 const db = require(__dirname + "/../_connect_db");
 var router = express.Router();
-
+//商品全部資料
 router.get("/", function(req, res) {
   const sql = "SELECT * FROM product";
   db.queryAsync(sql).then(result => {
     return res.json(result);
   });
 });
-
+//商品細節資料
+router.get("/:pId?",function(req,res){
+  const sql =`SELECT * FROM product WHERE pId = ?`
+  console.log(req.params.pId)
+  db.queryAsync(sql,[req.params.pId]).then(result=>{return res.json(result)})
+})
 module.exports = router;
