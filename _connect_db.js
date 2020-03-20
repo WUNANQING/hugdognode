a373f6c298fde0f2032fac30b7b2ce7f2858d08a
吳南慶 <wunanqing@wunanqingde-MacBook-Pro.local>
2020-03-21 00:44:04 +0800

    新增增orders路由;修改修改db帳密

--- _connect_db.js
+++ _connect_db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 const bluebird = require("bluebird");
 const db = mysql.createConnection({
   host: "localhost",
-  // user: "root",
-  // password: "root",
-  user: 'qazqaz',
-  password: 'qazqaz',
+  user: "root",
+  password: "root",
+  // user: 'qazqaz',
+  // password: 'qazqaz',
   database: "pet_db",
   dateStrings: true,
 });
--- routes/orders.js
+++ routes/orders.js
@@ -4,14 +4,15 @@ var router = express.Router();
 
 //新增訂單
 router.post('/post',(req,res)=>{
-    const sql =`INSERT INTO \`orders\`(\`name\`, \`address\`, \`zip\`, \`email\`, \`mobile\`, \`cardNumber\`, \`owner\`, \`created_at\`) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`
+    const sql = `INSERT INTO \`orders\`(\`name\`, \`address\`, \`zip\`, \`email\`, \`mobile\`, \`cardNumber\`, \`owner\`, \`cart\`, \`created_at\`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())`;
     db.queryAsync(sql,[req.body.lastName+req.body.firstName,
                        req.body.county+req.body.address+req.body.detailedAddress,
                        req.body.zip,
                        req.body.email,
                        req.body.mobile,
                        req.body.cardNumber,
-                       req.body.owner])
+                       req.body.owner,
+                       req.body.cart,])
         .then(result=>{
             return res.json(result);
         })
