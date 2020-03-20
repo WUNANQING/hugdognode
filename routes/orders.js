3f99de69f37c97ed159bedd657428764b83a52e5
WU NAN QING <a1993112@gmail.com>
2020-03-20 19:58:11 +0800

    新增order路由

---
+++ routes/orders.js
@@ -0,0 +1,23 @@
+var express = require("express");
+const db = require(__dirname + "/../_connect_db");
+var router = express.Router();
+
+//新增訂單
+router.post('/post',(req,res)=>{
+    const sql =`INSERT INTO \`orders\`(\`name\`, \`address\`, \`zip\`, \`email\`, \`mobile\`, \`cardNumber\`, \`owner\`, \`created_at\`) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`
+    db.queryAsync(sql,[req.body.lastName+req.body.firstName,
+                       req.body.county+req.body.address+req.body.detailedAddress,
+                       req.body.zip,
+                       req.body.email,
+                       req.body.mobile,
+                       req.body.cardNumber,
+                       req.body.owner])
+        .then(result=>{
+            return res.json(result);
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+})
+
+module.exports = router;
