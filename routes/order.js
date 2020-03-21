86e7486ecc16fd1d155104cf19435f128dfd2d42
吳南慶 <wunanqing@wunanqingde-MacBook-Pro.local>
2020-03-21 21:35:18 +0800

    products新增頁數和商品類別方法;order修改card和mid欄位

--- routes/order.js
+++ routes/order.js
@@ -3,27 +3,33 @@ const db = require(__dirname + "/../_connect_db");
 var router = express.Router();
 
 //新增訂單
-router.post('/post',(req,res)=>{
-    const sql = `INSERT INTO \`orders\`(\`name\`, \`address\`, \`zip\`, \`email\`, \`mobile\`, \`cardNumber\`, \`owner\`, \`cart\`, \`created_at\`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())`;
-    db.queryAsync(sql,[req.body.lastName+req.body.firstName,
-                       req.body.county+req.body.address+req.body.detailedAddress,
-                       req.body.zip,
-                       req.body.email,
-                       req.body.mobile,
-                       req.body.cardNumber,
-                       req.body.owner,
-                       req.body.cart,])
-        .then(result=>{
-            return res.json(result);
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-})
+router.post("/post", (req, res) => {
+  const sql = `INSERT INTO \`orders\`(\`name\`, \`address\`, \`zip\`, \`email\`, \`mobile\`, \`card\`, \`cardNumber\`, \`owner\`, \`cart\`, \`created_at\`, \`mId\`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?)`;
+  db.queryAsync(sql, [
+    req.body.lastName + req.body.firstName,
+    req.body.county + req.body.address + req.body.detailedAddress,
+    req.body.zip,
+    req.body.email,
+    req.body.mobile,
+    req.body.card,
+    req.body.cardNumber,
+    req.body.owner,
+    req.body.cart,
+    req.body.mId
+  ])
+    .then(result => {
+      return res.json(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 //訂單細節資料
-router.get("/:orderId?", function (req, res) {
-    const sql = `SELECT * FROM orders WHERE id = ?`
-    console.log(req.params.orderId)
-    db.queryAsync(sql, [req.params.orderId]).then(result => { return res.json(result) })
-})
+router.get("/:orderId?", function(req, res) {
+  const sql = `SELECT * FROM orders WHERE id = ?`;
+  console.log(req.params.orderId);
+  db.queryAsync(sql, [req.params.orderId]).then(result => {
+    return res.json(result);
+  });
+});
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,42 @@
 var express = require("express");
 const db = require(__dirname + "/../_connect_db");
+const url = require("url");
 var router = express.Router();
 //商品全部資料
-router.get("/", function(req, res) {
-  const sql = "SELECT * FROM product";
-  db.queryAsync(sql).then(result => {
+router.get("/:page?", function(req, res) {
+  //設定每頁商品筆數;總筆數;總頁數
+  const perPage = 9;
+  let totalRows, totalPages;
+  let page = req.params.page ? parseInt(req.params.page) : 1;
+  //得到總筆數
+  const t_sql = "SELECT COUNT(*) num FROM product";
+  db.queryAsync(t_sql)
+    .then(result => {
+      totalRows = result[0].num;
+      totalPages = Math.ceil(totalRows / perPage);
+      if (page < 1) page = 1;
+      if (page > totalPages) page = totalPages;
+      const sql = `SELECT * FROM \`product\` ORDER BY pId DESC LIMIT  ${(page -
+        1) *
+        perPage}, ${perPage}`;
+      return db.queryAsync(sql);
+    })
+    //node使用ejs不需要return但此處為前後端連接因此需要return
+    .then(result => {
+      return res.json({ totalRows, totalPages, page, rows: result });
+    });
+});
+//商品類別資料
+router.get("/?cName=", function(req, res) {
+  const q = url.parse(req.url, true);
+  console.log(q.path);
+});
+//商品細節資料
+router.get("/productDetail/:pId", function(req, res) {
+  console.log(req.params.pId);
+  const sql = `SELECT * FROM product WHERE pId = ?`;
+  db.queryAsync(sql, [req.params.pId]).then(result => {
     return res.json(result);
   });
 });
-//商品細節資料
-router.get("/:pId?",function(req,res){
-  const sql =`SELECT * FROM product WHERE pId = ?`
-  console.log(req.params.pId)
-  db.queryAsync(sql,[req.params.pId]).then(result=>{return res.json(result)})
-})
 module.exports = router;
