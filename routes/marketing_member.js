5eebe98bdd9da32b90eca8a9c1d92dc66dcc474d
Jason Tai <jasontai830118@gmail.com>
2020-03-25 16:19:03 +0800

    03250419

--- routes/marketing_member.js
+++ routes/marketing_member.js
@@ -29,11 +29,24 @@ router.get("/mmId/:mmId", function (req, res) {
   console.log(req.params.mmId)
   db.queryAsync(sql,[req.params.mmId]).then(result=>{return res.json(result)})
 });
+router.get("/used/:used/:mId", function (req, res) {
+  const sql = "SELECT * FROM marketing_member WHERE used = ? AND mId = ?";
+  console.log(req.params.used)
+  console.log(req.params.mId)
+  db.queryAsync(sql,[req.params.used,req.params.mId]).then(result=>{return res.json(result)})
+});
+router.get("/used/:mId", function (req, res) {
+  const sql = "SELECT * FROM marketing_member WHERE mId = ?";
+
+  console.log(req.params.mId)
+  db.queryAsync(sql,[req.params.mId]).then(result=>{return res.json(result)})
+});
 router.get("/used/:used", function (req, res) {
   const sql = "SELECT * FROM marketing_member WHERE used = ?";
   console.log(req.params.used)
   db.queryAsync(sql,[req.params.used]).then(result=>{return res.json(result)})
 });
+
 router.get("/code/:code", function (req, res) {
   const sql = "SELECT * FROM marketing_type WHERE code = ?";
   console.log(req.params.code)
